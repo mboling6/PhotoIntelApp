ec9896f3542c5f374e7d2024b0b54453504da7fc
Michael Boling <mboling6@gatech.edu>
2026-06-24 14:11:11 -0400

    Initial Commit

---
+++ PhotoIntelApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PhotoIntelApp
+//
+//  Created by Michael Boling on 6/24/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PhotoIntelApp/PhotoIntelAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoIntelAppApp.swift
+//  PhotoIntelApp
+//
+//  Created by Michael Boling on 6/24/26.
+//
+
+import SwiftUI
+
+@main
+struct PhotoIntelAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
